519d7a54a0456243b4c422e370bf4abca255d6f5
Raphael do Vale A. Gomes <raphael@dovale.net>
2018-10-29 00:42:29 -0300

    Database configuration

--- src/main/java/com/okta/developer/spring_boot_postgressql/SpringBootPostgressqlApplication.java
+++ src/main/java/com/okta/developer/spring_boot_postgressql/SpringBootPostgressqlApplication.java
@@ -2,8 +2,10 @@ package com.okta.developer.spring_boot_postgressql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringBootPostgressqlApplication {
 
     public static void main(String[] args) {
