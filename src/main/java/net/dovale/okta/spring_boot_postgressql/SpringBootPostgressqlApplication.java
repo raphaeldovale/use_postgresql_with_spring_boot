591cc70247cf02d41be27c105123e529ecbb2327
Raphael do Vale A. Gomes <raphael@dovale.net>
2018-10-28 22:49:55 -0300

    Sprint initialz

---
+++ src/main/java/net/dovale/okta/spring_boot_postgressql/SpringBootPostgressqlApplication.java
@@ -0,0 +1,12 @@
+package net.dovale.okta.spring_boot_postgressql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgressqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPostgressqlApplication.class, args);
+	}
+}
---
+++ src/test/java/net/dovale/okta/spring_boot_postgressql/SpringBootPostgressqlApplicationTests.java
@@ -0,0 +1,16 @@
+package net.dovale.okta.spring_boot_postgressql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootPostgressqlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
