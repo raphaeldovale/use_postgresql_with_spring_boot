353be4d0f073a18a5dc95ffa98165092909d26a7
Raphael do Vale A. Gomes <raphael@dovale.net>
2018-10-28 22:49:55 -0300

    Sprint initialz

---
+++ src/main/java/com/okta/developer/spring_boot_postgressql/SpringBootPostgressqlApplication.java
@@ -0,0 +1,12 @@
+package com.okta.developer.spring_boot_postgressql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgressqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPostgressqlApplication.class, args);
+    }
+}
