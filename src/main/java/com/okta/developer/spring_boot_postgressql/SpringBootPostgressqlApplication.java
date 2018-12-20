d99847284ba0c0545bf8cab11c9d787b9e2d5fe2
Raphael do Vale <raphael@dovale.net>
2018-12-20 02:55:44 -0200

    Add security

--- src/main/java/com/okta/developer/spring_boot_postgressql/SpringBootPostgressqlApplication.java
+++ src/main/java/com/okta/developer/spring_boot_postgressql/SpringBootPostgressqlApplication.java
@@ -2,6 +2,9 @@ package com.okta.developer.spring_boot_postgressql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -10,5 +13,17 @@ public class SpringBootPostgressqlApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootPostgressqlApplication.class, args);
+    }
+
+    @Configuration
+    static class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests().anyRequest().authenticated()
+                    .and()
+                    .oauth2ResourceServer().jwt();
+        }
     }
 }
