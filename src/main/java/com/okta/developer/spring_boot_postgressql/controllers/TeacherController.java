madz0/okta-postgresql-spring-boot-example

Using PostgreSQL's JSONB to store teacher's review

src/main/java/net/dovale/okta/spring_boot_postgressql/controllers/TeacherController.java
@@ -1,0 +1,36 @@
+package net.dovale.okta.spring_boot_postgressql.controllers;
+
+import net.dovale.okta.spring_boot_postgressql.entities.Review;
+import net.dovale.okta.spring_boot_postgressql.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityNotFoundException;
+
+@RestController
+public class TeacherController {
+
+    @Autowired
+    private final TeacherService teacherService;
+
+    @Autowired
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+
+    @PostMapping("/teachers/{id}/review")
+    public ResponseEntity addReview(@RequestBody Review review, @PathVariable("id") String teacherID){
+
+        try {
+            teacherService.addReview(teacherID, review);
+            return ResponseEntity.ok().build();
+        }
+        catch (EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+
+
+    }
+
+}

src/main/java/net/dovale/okta/spring_boot_postgressql/entities/Review.java
@@ -1,0 +1,18 @@
+package net.dovale.okta.spring_boot_postgressql.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review implements Serializable {
+    private String author;
+    private String review;
+    private LocalDate date;
+}

src/main/java/net/dovale/okta/spring_boot_postgressql/service/SimpleTeacherService.java
@@ -1,0 +1,48 @@
+package net.dovale.okta.spring_boot_postgressql.service;
+
+import net.dovale.okta.spring_boot_postgressql.dao.TeacherDAO;
+import net.dovale.okta.spring_boot_postgressql.entities.Review;
+import net.dovale.okta.spring_boot_postgressql.entities.Teacher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+public class SimpleTeacherService implements TeacherService {
+
+    private final TeacherDAO teacherDAO;
+
+    @Autowired
+    public SimpleTeacherService(TeacherDAO teacherDAO) {
+        this.teacherDAO = teacherDAO;
+    }
+
+    @Override
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public void addReview(String teacherID, Review review) {
+        Objects.requireNonNull(teacherID);
+        Objects.requireNonNull(review);
+
+        Teacher teacher = teacherDAO
+                .findById(UUID.fromString(teacherID))
+                .orElseThrow(() -> new EntityNotFoundException(teacherID));
+
+        review.setDate(LocalDate.now());
+
+        if(teacher.getReviews() == null){
+            teacher.setReviews(new ArrayList<>());
+        }
+
+        teacher.getReviews().add(review);
+
+        teacherDAO.save(teacher);
+
+    }
+}

src/main/java/net/dovale/okta/spring_boot_postgressql/service/TeacherService.java
@@ -1,0 +1,17 @@
+package net.dovale.okta.spring_boot_postgressql.service;
+
+import net.dovale.okta.spring_boot_postgressql.entities.Review;
+
+import javax.validation.constraints.NotNull;
+
+
+public interface TeacherService {
+
+    /**
+     *
+     * @param teacherID
+     * @param review
+     * @throws javax.persistence.EntityNotFoundException
+     */
+    void addReview(@NotNull String teacherID, @NotNull Review review);
+}

raphaeldovale/use_postgresql_with_spring_boot

src/main/java/com/okta/developer/spring_boot_postgressql/controllers/TeacherController.java
@@ -1,0 +1,36 @@
+package com.okta.developer.spring_boot_postgressql.controllers;
+
+import com.okta.developer.spring_boot_postgressql.entities.Review;
+import com.okta.developer.spring_boot_postgressql.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityNotFoundException;
+
+@RestController
+public class TeacherController {
+
+    @Autowired
+    private final TeacherService teacherService;
+
+    @Autowired
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+
+    @PostMapping("/teachers/{id}/review")
+    public ResponseEntity addReview(@RequestBody Review review, @PathVariable("id") String teacherID){
+
+        try {
+            teacherService.addReview(teacherID, review);
+            return ResponseEntity.ok().build();
+        }
+        catch (EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+
+
+    }
+
+}

src/main/java/com/okta/developer/spring_boot_postgressql/entities/Review.java
@@ -1,0 +1,18 @@
+package com.okta.developer.spring_boot_postgressql.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review implements Serializable {
+    private String author;
+    private String review;
+    private LocalDate date;
+}

src/main/java/com/okta/developer/spring_boot_postgressql/service/SimpleTeacherService.java
@@ -1,0 +1,48 @@
+package com.okta.developer.spring_boot_postgressql.service;
+
+import com.okta.developer.spring_boot_postgressql.dao.TeacherDAO;
+import com.okta.developer.spring_boot_postgressql.entities.Review;
+import com.okta.developer.spring_boot_postgressql.entities.Teacher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+public class SimpleTeacherService implements TeacherService {
+
+    private final TeacherDAO teacherDAO;
+
+    @Autowired
+    public SimpleTeacherService(TeacherDAO teacherDAO) {
+        this.teacherDAO = teacherDAO;
+    }
+
+    @Override
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public void addReview(String teacherID, Review review) {
+        Objects.requireNonNull(teacherID);
+        Objects.requireNonNull(review);
+
+        Teacher teacher = teacherDAO
+                .findById(UUID.fromString(teacherID))
+                .orElseThrow(() -> new EntityNotFoundException(teacherID));
+
+        review.setDate(LocalDate.now());
+
+        if(teacher.getReviews() == null){
+            teacher.setReviews(new ArrayList<>());
+        }
+
+        teacher.getReviews().add(review);
+
+        teacherDAO.save(teacher);
+
+    }
+}

src/main/java/com/okta/developer/spring_boot_postgressql/service/TeacherService.java
@@ -1,0 +1,17 @@
+package com.okta.developer.spring_boot_postgressql.service;
+
+import com.okta.developer.spring_boot_postgressql.entities.Review;
+
+import javax.validation.constraints.NotNull;
+
+
+public interface TeacherService {
+
+    /**
+     *
+     * @param teacherID
+     * @param review
+     * @throws javax.persistence.EntityNotFoundException
+     */
+    void addReview(@NotNull String teacherID, @NotNull Review review);
+}